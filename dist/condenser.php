925aa0a9fbd15d5e0397e1ba638bfad21ea9009b
SteGriff <github@stegriff.co.uk>
2016-11-25 15:37:19 +0000

    Trim condenser and fix comments

--- dist/condenser.php
+++ dist/condenser.php
@@ -15,38 +15,25 @@ foreach($files_to_condense as $filename)
 
 function condense($filename)
 {
-	//Read whole file and split into lines in OS-independent way
 	$content = file_get_contents($filename);
-	//$lines = preg_split("/\r\n|\n|\r/", $content);
-	
-	echo "Before:\r\n$content";
+
+	//Run regex replacing every included/required filename
+	// with the actual content of that file
+	//echo "Before:\r\n$content";
 	
 	$content = preg_replace_callback(
 		"/(include|require) ?\(?'?(.*\.php)'?\)? ?;/",
 		function($matches)
 		{
-			//Matches is like ["require 'credentials.php';", "require", "credentials.php"]
-			// So open the content of match no. 2
+			//$matches looks like ["require 'credentials.php';", "require", "credentials.php"]
+			// So treat match no. 2 as a filename and open it
 			return file_get_contents($matches[2]);
 		},
 		$content);
 	
-	echo "\r\n====\r\nAfter:\r\n$content";
+	//echo "\r\n====\r\nAfter:\r\n$content";
 	
-	// //Find include and require lines
-	// foreach($lines as $line)
-	// {
-		// //Look for keyword 'require' (PHP keywords are case insensitive)
-		// if (stripos($line, 'require') !== false || stripos($line, 'include') !== false)
-		// {
-			// echo "Parse '$line'\r\n";
-			// $included_file = preg_replace("/(include|require) ?\(?'?(.*\.php)'?\)? ?;?/", "$2", $line);
-			// echo "Got '$included_file'\r\n\r\n";
-			// $included_file_content = ;
-			
-		// }
-	// }
-	
+	//TODO Write $content back into the file
 }
 
 
