c0b38eadf4a90ab0b2b50e35e34a900268a63a8e
SteGriff <github@stegriff.co.uk>
2018-04-05 10:33:15 +0100

    Add logging

--- dist/config.php
+++ dist/config.php
@@ -10,4 +10,9 @@ function sh($commands)
 	return `2>&1 $commands`;
 }
 
+function info($logMsg)
+{
+	error_log("$logMsg\r\n", 3, "debug.log");
+}
+
 // Don't close this php block if you want to be able to use condenser.php
--- dist/deploy.php
+++ dist/deploy.php
@@ -8,6 +8,8 @@ header("content-type: text/plain");
 require 'credentials.php';
 require 'config.php';
 
+info("Deploy started");
+
 $is_post = $_SERVER['REQUEST_METHOD'] === 'POST';
 $headers = apache_request_headers();
 $supplied_key = null;
@@ -15,6 +17,8 @@ $supplied_key = null;
 //Decide how we're going to get the secret deployment key
 if ($is_post && isset($headers['X-Hub-Signature']))
 {
+	info("Option 1: POST with X-Hub-Signature");
+	
 	/* [Option 1] Handle a POST with X-Hub-Signature */
 	// Looks like "sha1=f83e18d43dde7249a5980f44bf89c5735273fe06"
 	$supplied_key = $headers['X-Hub-Signature'];
@@ -32,21 +36,32 @@ if ($is_post && isset($headers['X-Hub-Signature']))
 }
 else if ($allow_get === true)
 {
+	info("Option 2: GET with key");
+	
 	/* [Option 2] Handle a GET with key, for clients that don't support X-Hub-Signature */
 	$supplied_key = $_GET['key'];
 }
 else
 {
+	info("Deploy not allowed");
 	echo "No X-Hub-Signature was received in POST request, and GET is disabled. Deployment impossible!";
 }
 
+info("Credentials are ready...");
+
 if ($supplied_key == $deployment_key)
 {
+	info("Doing git pull...");
 	$output = sh("$git pull");
 	echo $output;
+	info("Finished git; see next line for output:");
+	info($output);
 }
 else
 {
+	info("Deployment key incorrect");
 	echo "Deployment key incorrect ($supplied_key)";
 }
+
+info("End");
 ?>
